e03739207f202a62581f5da315a1aa9287dcd2b9
thanhtungct7 <ct070358@actvn.edu.vn>
2026-06-18 14:31:23 +0700

    fix: sudo part 1 timemanegement

--- part1/timemanagement.sh
+++ part1/timemanagement.sh
@@ -7,31 +7,16 @@ do
     case $choice in
         1) TZ="Asia/Ho_Chi_Minh" date;;
         2)
-            if [ `whoami` != "root" ]
-            then
-                echo 'You need to run this command as root to set the time.'
-            else
-                echo "Enter the time in the format hh:mm:ss:"
-                read new_time
-                date -s "${new_time}"
-            fi;;
+            echo "Enter the time in the format hh:mm:ss:"
+            read new_time
+            sudo date -s "${new_time}";;
         3)
-            if [ `whoami` != "root" ]
-            then
-                echo 'You need to run this command as root to set the date.'
-            else
-                echo "Enter the date in the format yyyy-mm-dd:"
-                read new_time
-                date  -s "${new_time}"
-            fi;;
+            echo "Enter the date in the format yyyy-mm-dd:"
+            read new_time
+            sudo date -s "${new_time}";;
         4)
-            if [ `whoami` != "root" ]
-            then
-                echo 'You need to run this command as root to update the time.'
-            else
-                echo "Updating time from Microsoft server..."
-                sntp -sS time.windows.com
-            fi;;
+            echo "Updating time from Microsoft server..."
+            sudo sntp -sS time.windows.com;;
         5) echo "Exiting..."; exit;;
         *) echo "Invalid choice";;
     esac
